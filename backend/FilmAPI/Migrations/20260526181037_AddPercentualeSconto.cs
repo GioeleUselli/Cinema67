407a9c0fe8c0b04f36a0a043f4b5ec6ed53907d8
Gioele Uselli <gioele.uselli@issgreppi.it>
2026-05-26 20:10:43 +0200

    fix: migration PercentualeSconto

---
+++ backend/FilmAPI/Migrations/20260526181037_AddPercentualeSconto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPercentualeSconto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PercentualeSconto",
+                table: "Premi",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PercentualeSconto",
+                table: "Premi");
+        }
+    }
+}
