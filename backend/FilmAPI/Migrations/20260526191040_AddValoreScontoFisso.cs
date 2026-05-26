33d6f280b2de06821383f07933717858196c7415
Gioele Uselli <gioele.uselli@issgreppi.it>
2026-05-26 21:10:46 +0200

    fix: migration ValoreScontoFisso

---
+++ backend/FilmAPI/Migrations/20260526191040_AddValoreScontoFisso.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddValoreScontoFisso : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValoreScontoFisso",
+                table: "MerchDiscountCodes",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ValoreScontoFisso",
+                table: "MerchDiscountCodes");
+        }
+    }
+}
