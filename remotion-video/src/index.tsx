ec5ddb3e5e7f8e50e7fcc432771734e2fd4b52b5
Gioele Uselli <gioele.uselli@issgreppi.it>
2026-06-04 11:52:49 +0200

    Fix: Remotion registerRoot per build MP4

--- remotion-video/src/index.tsx
+++ remotion-video/src/index.tsx
@@ -1,18 +1,19 @@
-import { Composition } from "remotion";
+import { Composition, registerRoot } from "remotion";
 import { Cinema67Presentation } from "./Root";
 
-export const RemotionRoot: React.FC = () => {
+const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
         id="Cinema67Presentation"
         component={Cinema67Presentation}
-        durationInFrames={30 * 60 * 3.5} // ~3.5 minutes at 30fps
+        durationInFrames={30 * 60 * 3.5}
         fps={30}
         width={1920}
         height={1080}
-        defaultProps={{}}
       />
     </>
   );
 };
+
+registerRoot(RemotionRoot);
